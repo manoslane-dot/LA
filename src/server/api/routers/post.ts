ca4bcab99aa208ec13bc5500c2b45a9e88b69c12
manoslane-dot <manoslane@gmail.com>
2026-07-23 11:11:26 +0300

    Add getLatest and create procedures to postRouter

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,24 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure.query(() => {
     return "Hello from tRPC";
   }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.db.post.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.post.create({
+        data: {
+          name: input.name,
+        },
+      });
+    }),
 });
