0839499f2ebd64206f1a85835a07ac049464e216
manoslane-dot <manoslane@gmail.com>
2026-07-23 13:56:00 +0300

    Refactor postRouter to include greeting input

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,24 +1,28 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure.query(() => {
-    return "Hello from tRPC";
-  }),
-
-  getLatest: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
 
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      return await ctx.db.post.create({
+      return ctx.db.post.create({
         data: {
           name: input.name,
         },
       });
     }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.post.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
 });
